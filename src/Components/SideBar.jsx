b15de793a335e96312f330e67561f8408e084bd2
RuslanDidun <ruslandidun5@gmail.com>
2022-12-30 12:54:16 -0700

    add styles, Videos component

--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -1,4 +1,5 @@
-import { Stack } from '@mui/material';
+import React from 'react';
+import { Stack, Button } from '@mui/material';
 import { categories } from '../utils/constants';
 
 const selectedCategory = 'New'
@@ -14,14 +15,15 @@ const SideBar = () => (
   >
     {categories.map((category) => (
       <button
-        className="category-btn"
-        style={{
-          background: category.name === selectedCategory && '#FC1503',
-          color: 'white'
-        }}
-        key={category.name}>
-        <span style={{ color: category.name === selectedCategory ? 'white' : 'red', marginRight: '15px' }}>{category.icon}</span>
-        <span style={{ opacity: category.name === selectedCategory ? '1' : '0.8' }}>{category.name}</span>
+      className="category-btn"
+      style={{
+        background: category.name === selectedCategory && '#FC1503',
+        color: 'white'
+      }}
+      key={category.name}
+      >
+        <span style={{ color: category.name===selectedCategory ? 'white': 'red', marginRight: '15px'}}>{category.icon}</span>
+        <span style={{ opacity: category.name === selectedCategory ? '1': '0.8'}}>{category.name}</span>
       </button>
     ))}
   </Stack>

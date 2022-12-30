b0eb3638f0f2bc0cccc036fb21aead852f908d5b
RuslanDidun <ruslandidun5@gmail.com>
2022-12-30 11:37:57 -0700

    add SearchBar, SideBar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Stack } from "@mui/material";
 import { Link } from "react-router-dom";
-import { logo } from '../utils/Constants';
+import { logo } from '../utils/constants';
 import SearchBar from './SearchBar';
 
 const Navbar = () => (

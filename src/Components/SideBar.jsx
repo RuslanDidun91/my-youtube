c19de2ab78b5b6b33b0100c97af6ae762eba31d5
RuslanDidun <ruslandidun5@gmail.com>
2022-12-30 21:13:16 -0700

    fetch data, display video

--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import { Stack, Button } from '@mui/material';
+import { Stack } from '@mui/material';
 import { categories } from '../utils/constants';
 
-const selectedCategory = 'New'
 
-const SideBar = () => (
+const SideBar = ({selectedCategory, setSelectedCategory}) => (
   <Stack
     direction="row"
     sx={{
@@ -16,6 +15,7 @@ const SideBar = () => (
     {categories.map((category) => (
       <button
       className="category-btn"
+      onClick={() => setSelectedCategory(category.name)}
       style={{
         background: category.name === selectedCategory && '#FC1503',
         color: 'white'

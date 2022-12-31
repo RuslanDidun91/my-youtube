04f05fe54d64b8a04cde815d845a2ac452d7eeaa
RuslanDidun <ruslandidun5@gmail.com>
2022-12-31 09:47:22 -0700

    add vieoCard component

--- src/Components/Feed.jsx
+++ src/Components/Feed.jsx
@@ -1,14 +1,12 @@
 import React from "react";
-import { useState, useEffect } from 'react';
-import { Box, Stack, Typography } from '@mui/material';
-import { SideBar, Videos } from './';
+import { useState, useEffect } from "react";
+import { Box, Stack, Typography } from "@mui/material";
+import { SideBar, Videos } from "./";
 import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 const Feed = () => {
-
-  const [selectedCategory, setSelectedCategory] = useState('New')
-  const [videos, setVideos] = useState([])
-
+  const [selectedCategory, setSelectedCategory] = useState("New");
+  const [videos, setVideos] = useState([]);
   useEffect(() => {
     fetchFromAPI(`search?part=snippet&q=${selectedCategory}`).then((data) =>
       setVideos(data.items)
@@ -16,29 +14,40 @@ const Feed = () => {
   }, [selectedCategory]);
 
   return (
-    <Stack sx={{ flexDirection: { sx: 'column', md: 'row' } }}>
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
       <Box
         sx={{
           height: { sx: "auto", md: "92vh" },
           borderRight: "1px solid #3d3d3d",
-          px: { sx: 0, md: 2 }
-        }}>
-        <SideBar selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
+          px: { sx: 0, md: 2 },
+        }}
+      >
+        <SideBar
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+        />
         <Typography
           className="copyright"
           variant="body2"
-          sx={{ mt: 1.5, color: '#fff' }}>
-          Copyright 2023 Ruslan Didun
+          sx={{ mt: 1.5, color: "#fff" }}
+        >
+          CopyRight 2022 Robert Neyrinck
         </Typography>
       </Box>
-      <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2 }}>
-        <Typography variant='h4' fontWeight="bold" mb={2} sx={{ color: 'white' }}>
-          {selectedCategory}<span style={{ color: '#F31503' }}>Videos</span>
+      <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          mb={2}
+          sx={{ color: "white" }}
+        >
+          {selectedCategory}
+          <span style={{ color: "#Fc1503" }}> videos</span>
         </Typography>
         <Videos videos={videos} />
       </Box>
     </Stack>
-  )
-}
+  );
+};
 
 export default Feed;
--- src/Components/VideoCard.jsx
+++ src/Components/VideoCard.jsx
@@ -1,11 +1,55 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { Typography, Card, CardContent, CardMedia } from "@mui/material";
+import { CheckCircle } from "@mui/icons-material";
 
-const VideoCard = () => {
-  return(
-    <div>
-      videocard
-    </div>
-  )
-}
+import {
+  demoThumbnailUrl,
+  demoVideoUrl,
+  demoVideoTitle,
+  demoChannelUrl,
+  demoChannelTitle,
+} from "../utils/constants";
+
+const VideoCard = ({
+  video: {
+    id: { videoId },
+    snippet
+  }
+}) => (
+    <Card
+      sx={{
+        width: { xs: "100%", sm:'358px', md: "320px" },
+        boxShadow: "none",
+        borderRadius: 0,
+      }}
+    >
+      <Link to={videoId ? `/video/${videoId}` : `/video/cV2gBU6hKfY`}>
+      <CardMedia image={snippet?.thumbnails?.high?.url || demoThumbnailUrl} alt={snippet?.title} 
+        sx={{ width: { xs: '100%', sm: '358px'}, height: 180 }} 
+      />
+      </Link>
+      <CardContent sx={{ backgroundColor: "#1e1e1e", height: "106px" }}>
+        <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+          <Typography variant="subtitle1" fontWeight="bold" color="#FFF">
+            {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}
+          </Typography>
+        </Link>
+        <Link
+          to={
+            snippet?.channelId
+              ? `/channel/${snippet?.channelId}`
+              : demoChannelUrl
+          }
+        >
+          <Typography variant="subtitle2" color="#808080">
+            {snippet?.channelTitle || demoChannelTitle}
+            <CheckCircle sx={{ fontSize: 12, color: "#808080", ml: '5px' }} />
+          </Typography>
+        </Link>
+      </CardContent>
+    </Card>
+  );
+
 
 export default VideoCard;

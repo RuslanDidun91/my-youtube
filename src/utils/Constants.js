b1caeedb2d424c523a47fdbe7f6eb8a424139642
RuslanDidun <ruslandidun5@gmail.com>
2023-01-02 10:48:52 -0700

    polishing, add readme

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -19,7 +19,7 @@ export const categories = [
   { name: 'JS Mastery', icon: <CodeIcon />, },
   { name: 'Coding', icon: <CodeIcon />, },
   { name: 'ReactJS', icon: <CodeIcon />, },
-  // { name: 'NextJS', icon: <CodeIcon />, },
+  { name: 'NextJS', icon: <CodeIcon />, },
   { name: 'Music', icon: <MusicNoteIcon /> },
   { name: 'Education', icon: <SchoolIcon />, },
   { name: 'Podcast', icon: <GraphicEqIcon />, },

ae40f7ead434c8cdfa3209e8a9a21958f38e1885
RuslanDidun <ruslandidun5@gmail.com>
2023-01-01 20:03:28 -0700

    implement search feed

--- src/Components/SearchFeed.jsx
+++ src/Components/SearchFeed.jsx
@@ -1,31 +1,28 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import { Box, Stack, Typography } from "@mui/material";
-import { SideBar, Videos } from "./";
+import { Box, Typography } from "@mui/material";
+import { Videos } from "./";
 import { fetchFromAPI } from "../utils/fetchFromAPI";
+import { useParams } from "react-router-dom";
+
 
 const Feed = () => {
-  const [selectedCategory, setSelectedCategory] = useState("New");
   const [videos, setVideos] = useState([]);
+  const { searchTerm } = useParams()
   useEffect(() => {
-    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`).then((data) =>
+    fetchFromAPI(`search?part=snippet&q=${searchTerm}`).then((data) =>
       setVideos(data.items)
     );
-  }, [selectedCategory]);
+  }, [searchTerm]);
 
   return (
     <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
-    <Typography
-      variant="h4"
-      fontWeight="bold"
-      mb={2}
-      sx={{ color: "white" }}
-    >
-      {selectedCategory}
-      <span style={{ color: "#Fc1503" }}> videos</span>
-    </Typography>
-    <Videos videos={videos} />
-  </Box>
+      <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
+        Search Results for:  
+        <span style={{ color: "#F31503" }}> {searchTerm}</span>
+      </Typography>
+      <Videos videos={videos} />
+    </Box>
   );
 };
 

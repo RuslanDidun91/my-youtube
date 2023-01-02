068b7a4226598ded9e0bbae0926336bc5fe34752
RuslanDidun <ruslandidun5@gmail.com>
2023-01-01 20:12:16 -0700

    search feed

--- src/Components/SearchFeed.jsx
+++ src/Components/SearchFeed.jsx
@@ -8,7 +8,8 @@ import { useParams } from "react-router-dom";
 
 const Feed = () => {
   const [videos, setVideos] = useState([]);
-  const { searchTerm } = useParams()
+  const { searchTerm } = useParams();
+
   useEffect(() => {
     fetchFromAPI(`search?part=snippet&q=${searchTerm}`).then((data) =>
       setVideos(data.items)
@@ -18,7 +19,7 @@ const Feed = () => {
   return (
     <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
       <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
-        Search Results for:  
+        Results for:
         <span style={{ color: "#F31503" }}> {searchTerm}</span>
       </Typography>
       <Videos videos={videos} />
